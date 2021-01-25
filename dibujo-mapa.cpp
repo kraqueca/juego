c96841f49e56b6af6b6682cd71ef0af621f5c331
kraqueca <raquec60@gmail.com>
2021-01-24 18:18:22 -0600

    Moviendo al personaje arriba y abajo

--- dibujo-mapa.cpp
+++ dibujo-mapa.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void drawMap(int heroPos, char gameMap[5]){
+void drawMap(int heroPosX, int heroPosY, char gameMap[5][5]){
     for (int i = 0; i < 5; i++){
-        if(i != heroPos){
-            cout << gameMap [i];
-        }else{
-            cout << 'H';
-        }
+            for (int p = 0; p < 5; p++){
+            if(i != heroPosY){
+                cout << gameMap [p][i];
+            }else{
+                if (p != heroPosX){
+                    cout << gameMap [p][i];
+                }else{
+                    cout << 'H';   
+                }
+            }
+        }   
+        cout << endl;
     }
 }
 
 int main (){
-    int heroPos = 1;
+    int heroPosX = 1;
+    int heroPosY = 1;
     bool isGameOver = false;
     char input = ' ';
-    char gameMap[5] = {'1', '1', '1', '1', '1'};
-    drawMap (heroPos, gameMap);
+    char gameMap[5][5] = {
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'},
+        {'1', '1', '1', '1', '1'}
+    };
+    drawMap (heroPosX, heroPosY, gameMap);
 
     while (isGameOver == false){
         cin >> input;
         if (input == 'd'){
-            heroPos += 1;
+            heroPosX += 1;
         }else if(input == 'a'){
-            heroPos -= 1;
+            heroPosX -= 1;
+        }else if (input == 'w'){
+            heroPosY -= 1;
+        }else if(input == 's'){
+            heroPosY += 1;
         }else if(input == 'p'){
             isGameOver = true;
         }
-        drawMap (heroPos, gameMap);
+        drawMap (heroPosX, heroPosY, gameMap);
     }
     return 0;
 }
